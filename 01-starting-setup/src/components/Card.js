b9e49be900a717b947543dfdc16061a71c3eaad7
wcqrwtqr <mohalbatati@hotmail.com>
2022-03-21 23:15:38 +0300

    finished first part

--- 01-starting-setup/src/components/Card.js
+++ 01-starting-setup/src/components/Card.js
@@ -1,7 +1,8 @@
 import './Card.css'
-function Card(){
+function Card(props){
+  const classes = 'card ' + props.className
   return (
-    <div className='card'> </div>
+    <div className={classes}>{props.children}</div>
   )
 }
 export default Card;
