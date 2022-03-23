408e1f9ccae4f9a975ab954212559a9191195da0
wcqrwtqr <mohalbatati@hotmail.com>
2022-03-23 23:59:00 +0300

    finished the assignment to pass data state lift

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react'
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
+import ExpensesFitler from './ExpensesFilter'
 import Card from '../UI/Card'
 
 const Expenses = (props) => {
-  return(
-    <Card className='expenses'>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}/>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}/>
-      <ExpenseItem 
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}/>
-      <ExpenseItem 
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}/>
-    </Card>
+
+  const [filteredYear, setSelectedYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear)
+    console.log(`one level up :: Year selected ${selectedYear}`)
+  }
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFitler selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date} />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date} />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date} />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date} />
+      </Card>
+    </div>
   )
 }
 
