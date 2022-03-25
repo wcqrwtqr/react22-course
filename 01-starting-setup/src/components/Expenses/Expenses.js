bb8852494990cab8b65f26d362bdf79c2390897b
wcqrwtqr <mohalbatati@hotmail.com>
2022-03-25 21:49:01 +0300

    beore the filter challenge

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
 import ExpensesFitler from './ExpensesFilter'
@@ -17,23 +17,16 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFitler selected={filteredYear}
-                        onChangeFilter={filterChangeHandler}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date} />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date} />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date} />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date} />
+          onChangeFilter={filterChangeHandler} />
+
+        {props.items.map(el => (
+          <ExpenseItem
+            key={el.id}
+            title={el.title}
+            amount={el.amount}
+            date={el.date} />
+        ))}
+
       </Card>
     </div>
   )
